58528cdcc773c5be9fc983d7319d896e6884eae3
onewillow <210259229+onewillowbrook@users.noreply.github.com>
2025-05-19 15:30:16 -0400

    Building logic for picking a number

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
 console.log("Hello world!");
+
+function getComputerChoice() {
+    let decider = "";
+    let yoNumber = Math.random();
+    if (yoNumber <= .33) {
+        decider = "rock"
+    } else if (yoNumber <=.66) {
+        decider = "paper"
+    } else {
+        decider = "scissors"
+    }
+    console.log(yoNumber)
+    console.log(decider)
+    
+
+}
+
+
+console.log(getComputerChoice());
+
+// console.log(Math.random());
